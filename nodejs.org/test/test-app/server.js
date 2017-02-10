3f2a9460c4e4f7ca5699cc94b4f3c7cfbf94fc9f
Helio Frota <00hf11@gmail.com>
2017-02-09 22:04:42 -0300

    build: test app node-echo

--- nodejs.org/test/test-app/server.js
+++ nodejs.org/test/test-app/server.js
@@ -1,50 +1,58 @@
-var util = require('util');
-var http = require('http');
-var url = require('url');
-var qs = require('querystring');
-var os = require('os')
-var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-var nodeEnv = process.env.NODE_ENV || 'unknown';
-var server = http.createServer(function (req, res) {
-	var url_parts = url.parse(req.url, true);
+'use strict';
 
-	var body = '';
-	req.on('data', function (data) {
-		body += data;
-	});
-	req.on('end', function () {
-		var formattedBody = qs.parse(body);
+const util = require('util');
+const http = require('http');
+const qs = require('querystring');
+const os = require('os');
 
-		res.writeHead(200, {'Content-Type': 'text/plain'});
+const port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-		res.write('This is a node.js echo service\n');
-		res.write('Host: ' + req.headers.host + '\n');
-		res.write('\n');
-		res.write('node.js Production Mode: ' + (nodeEnv == 'production' ? 'yes' : 'no') + '\n');
-		res.write('\n');
-		res.write('HTTP/' + req.httpVersion +'\n');
-		res.write('Request headers:\n');
-		res.write(util.inspect(req.headers, null) + '\n');
-		res.write('Request query:\n');
-		res.write(util.inspect(url_parts.query, null) + '\n');
-		res.write('Request body:\n');
-		res.write(util.inspect(formattedBody, null) + '\n');
-		res.write('\n');
-		res.write('Host: ' + os.hostname() + '\n');
-		res.write('OS Type: ' + os.type() + '\n');
-		res.write('OS Platform: ' + os.platform() + '\n');
-		res.write('OS Arch: ' + os.arch() + '\n');
-		res.write('OS Release: ' + os.release() + '\n');
-		res.write('OS Uptime: ' + os.uptime() + '\n');
-		res.write('OS Free memory: ' + os.freemem() / 1024 / 1024 + 'mb\n');
-		res.write('OS Total memory: ' + os.totalmem() / 1024 / 1024 + 'mb\n');
-		res.write('OS CPU count: ' + os.cpus().length + '\n');
-		res.write('OS CPU model: ' + os.cpus()[0].model + '\n');
-		res.write('OS CPU speed: ' + os.cpus()[0].speed + 'mhz\n');
-		res.end('\n');
+let nodeEnv = process.env.NODE_ENV || 'unknown';
 
-	});
+const server = http.createServer((req, res) => {
+  const query = require('url').parse(req.url, true).query;
+
+  let body = '';
+
+  req.on('data', data => {
+    body += data;
+  });
+
+  req.on('end', () => {
+    const formattedBody = qs.parse(body);
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    let out = `This is a node.js echo service
+Host: ${req.headers.host}
+
+node.js Production Mode: ${nodeEnv == 'production' ? 'yes' : 'no'}
+
+HTTP/${req.httpVersion}
+Request headers:
+${util.inspect(req.headers, null)}
+Request query:
+${util.inspect(query, null)}
+Request body:
+${util.inspect(formattedBody, null)}
+Host: ${os.hostname()}
+OS Type: ${os.type()}
+OS Platform: ${os.platform()}
+OS Arch: ${os.arch()}
+OS Release: ${os.release()}
+OS Uptime: ${os.uptime()}
+OS Free memory: ${os.freemem() / 1024 / 1024}mb
+OS Total memory: ${os.totalmem() / 1024 / 1024}mb
+OS CPU count: ${os.cpus().length}
+OS CPU model: ${os.cpus()[0].model}
+OS CPU speed: ${os.cpus()[0].speed}mhz
+`;
+
+		res.end(out);
+  });
 });
+
 server.listen(port);
-console.log('Server running on ' + ip + ':' + port);
+
+console.log(`Server running on ${ip}:${port}`);
